dfe927347365e209c7286a1788584d788ac1fd64
VIGNESH <111691971+RM-VIGNESH@users.noreply.github.com>
2022-08-26 21:11:47 +0530

    Update EVEN_DIGITS.java

--- EVEN_DIGITS.java
+++ EVEN_DIGITS.java
@@ -1,7 +1,7 @@
 package academy.learnprogramming;
 
 public class EVEN_DIGITS {
-    // driver code
+    // driver code....
     public static void main(String[] args) {
         int[] nums ={12,345,2,6,7896};
         System.out.println(findNumbers(nums));
